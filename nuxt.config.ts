3a878959246661eb23cf246189bafcf951c167cd
Konkamon Sion <bigkonkamon@gmail.com>
2025-05-16 12:26:51 +0700

    feat(UI): :lipstick: ปรับให้ Icon ใช้ API ภายนอก

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -33,6 +33,9 @@ export default defineNuxtConfig({
       Kanit: [300, 400, 500, 600, 700, 800],
     },
   },
+  icon: {
+    provider: 'iconify',
+  },
   image: {
     cloudinary: {
       baseURL: 'https://res.cloudinary.com/dqx4sss9s/image/upload',
