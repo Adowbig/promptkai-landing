f994abb5487c6922bacf70209b10ab8666933b07
Konkamon Sion <bigkonkamon@gmail.com>
2025-06-19 23:11:50 +0700

    feat(CTA): :sparkles: Improve layout / Reuse AnimatedBG / use remote image

--- app/components/Home/CTA.vue
+++ app/components/Home/CTA.vue
@@ -1,24 +1,25 @@
 <template>
   <section class="relative border-primary-300 border-y dark:border-primary-600 overflow-hidden">
-    <div class="top-12 -left-10 -z-10 absolute bg-primary opacity-50 blur-[200px] rounded-full size-[500px]" />
-    <div class="top-14 -right-10 -z-10 absolute bg-primary opacity-50 blur-[200px] rounded-full size-[500px]" />
+    <LazyAnimatedBackground />
     <UContainer>
-      <div class="flex justify-between items-center">
-        <div>
-          <h1 class="mb-2 font-bold text-highlighted text-5xl">
+      <div class="flex lg:flex-row flex-col lg:justify-between items-center pt-16 lg:pt-0 text-center lg:text-start">
+        <div class="mb-8 lg:mb-0">
+          <h1 class="mb-2 font-extrabold text-highlighted text-3xl md:text-5xl">
             ดาวน์โหลด <span class="text-primary">พร้อมขาย</span> เลย
           </h1>
-          <p class="text-muted dark:text-toned text-lg">
+          <p class="text-muted dark:text-toned md:text-lg">
             ดาวน์โหลดแอพพลิเคชัน <span class="text-primary">พร้อมขาย</span> เพื่อเริ่มต้นใช้งานระบบ POS บนมือถือ
           </p>
           <DownloadButton
             size="xl"
             class="my-5"
+            app-name
           />
         </div>
         <NuxtImg
-          class="w-64 h-auto"
-          src="/phoneMockup.png"
+          provider="cloudinary"
+          class="w-2xs md:w-sm"
+          src="phoneMockup_fmiixn"
         />
       </div>
     </UContainer>
