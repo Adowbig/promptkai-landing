9dbbdf423936fcf46db74c05a01119ba50a4daab
Konkamon Sion <bigkonkamon@gmail.com>
2025-06-19 23:08:11 +0700

    feat(DownloadButton): :sparkles: Add confetti effert on button click

--- app/components/DownloadButton.vue
+++ app/components/DownloadButton.vue
@@ -6,12 +6,14 @@
     :icon="$props.icon"
     target="_blank"
     :size="size"
+    @click="triggerConfetti($event)"
   />
 </template>
 
 <script setup lang="ts">
 import type { button } from '#build/ui'
 
+const appConfig = useAppConfig()
 defineProps({
   appName: {
     type: Boolean,
@@ -30,4 +32,22 @@ defineProps({
     default: '',
   },
 })
+
+const triggerConfetti = (event: Event) => {
+  const rect = (event.target as HTMLElement).getBoundingClientRect()
+  const x = (rect.left + rect.width / 2) / window.innerWidth
+  const y = (rect.top + rect.height / 2) / window.innerHeight
+
+  useConfetti({
+    colors: [
+      getComputedStyle(document.documentElement).getPropertyValue(`--color-${appConfig.ui.colors.primary}-600`),
+      getComputedStyle(document.documentElement).getPropertyValue(`--color-${appConfig.ui.colors.primary}-300`),
+    ],
+    particleCount: 100,
+    spread: 360,
+    startVelocity: 20,
+    ticks: 100,
+    origin: { x, y },
+  })
+}
 </script>
